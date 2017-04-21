fd5b363c9668efff4d48a78d8f4a54f34773e766
David Hu <davidhu314@gmail.com>
2017-04-21 14:29:18 -0700

    set up github oauth

--- src/components/splash.jsx
+++ src/components/splash.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { loginUser } from '../actions/oauth_actions';
+import { authenticateGithub } from '../util/github_oauth';
 
 class Splash extends React.Component {
   constructor(props) {
@@ -10,6 +11,7 @@ class Splash extends React.Component {
   render() {
     return (
       <div>
+        <div onClick={authenticateGithub}>Github</div>
         NOTI!
         Sign Up.
         <div onClick={this.props.loginUser}>Log In.</div>

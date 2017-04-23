ca97b7381ab41e799d9f1515343dbbe5ebbb7dff
David Hu <davidhu314@gmail.com>
2017-04-22 18:35:42 -0700

    add ipc listener to update icon if there are notifications

--- main.js
+++ main.js
@@ -4,99 +4,108 @@ const url = require('url');
 
 const iconTemplatePath = path.join(__dirname, 'app', 'assets', 'iconTemplate.png');
 const iconHightlightPath = path.join(__dirname, 'app', 'assets', 'iconHighlight.png');
-let appIcon = null;
-let win = null;
 
-const initializeMenu = () => {
-  let template = [
-    {
-      label: 'Mute'
-    },
-    {
-      label: 'Settings',
-      accelerator: "Command+,",
-      click: function() {
-        appIcon.window.show();
+app.on('ready', () => {
+  
+  let appIcon = null;
+  let win = null;
+
+  const initializeMenu = () => {
+    let template = [
+      {
+        label: 'Mute'
+      },
+      {
+        label: 'Settings',
+        accelerator: "Command+,",
+        click: function() {
+          appIcon.window.show();
+        }
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator: 'Alt+Command+I',
+        click: function() {
+          appIcon.window.show();
+          appIcon.window.toggleDevTools();
+        }
+      },
+      { 
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        selector: 'terminate:',
       }
-    },
-    {
-      label: 'Toggle DevTools',
-      accelerator: 'Alt+Command+I',
-      click: function() {
-        appIcon.window.show();
-        appIcon.window.toggleDevTools();
-      }
-    },
-    { 
-      label: 'Quit',
-      accelerator: 'Command+Q',
-      selector: 'terminate:',
-    }
-  ];
+    ];
 
-  appIcon = new Tray(iconTemplatePath);
-  appIcon.setPressedImage(iconHightlightPath);
+    appIcon = new Tray(iconTemplatePath);
 
-  var contextMenu = Menu.buildFromTemplate(template);
+    var contextMenu = Menu.buildFromTemplate(template);
 
-  appIcon.setToolTip('All your notifications on your menu bar.');
-  appIcon.setContextMenu(contextMenu);
+    appIcon.setToolTip('All your notifications on your menu bar.');
+    appIcon.setContextMenu(contextMenu);
 
-};
-
-const hideWindow = () => {
-  if(!appIcon.window) { return; }
-  appIcon.window.hide();
-};
-
-const showWindow = () => {
-  if(appIcon.window) { return; }
-  appIcon.window.show();
-};
-
-const createWindow = () => {
-  initializeMenu();
-
-  let windowParams = {
-    width: 400,
-    height: 600,
-    resizeable: false,
-    show: false,
-    webPreferences: {
-      overlayScrollbars: true
-    }
   };
 
-  appIcon.window = new BrowserWindow(windowParams);
+  const hideWindow = () => {
+    if(!appIcon.window) { return; }
+    appIcon.window.hide();
+  };
 
-  appIcon.window.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+  const showWindow = () => {
+    if(appIcon.window) { return; }
+    appIcon.window.show();
+  };
 
-  // appIcon.window.on('blur', hideWindow);
+  const createWindow = () => {
+    initializeMenu();
 
-  appIcon.window.webContents.openDevTools();
+    let windowParams = {
+      width: 400,
+      height: 600,
+      resizeable: false,
+      show: false,
+      webPreferences: {
+        overlayScrollbars: true
+      }
+    };
 
-  appIcon.window.on('closed', () => {
-    appIcon.window = null;
-  });
+    appIcon.window = new BrowserWindow(windowParams);
 
-  appIcon.window.webContents.on('devtools-opened', (event, deviceList, callback) => {
-    appIcon.window.setSize(800, 600);
-    appIcon.window.setResizable(true);
-  });
+    appIcon.window.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
 
-  appIcon.window.webContents.on('devtools-closed', (event, deviceList, callback) => {
-    appIcon.window.setSize(400, 600);
-    appIcon.window.setResizable(false);
-  });
+    // appIcon.window.on('blur', hideWindow);
 
-};
+    appIcon.window.webContents.openDevTools();
 
+    appIcon.window.on('closed', () => {
+      appIcon.window = null;
+    });
 
-app.on('ready', createWindow);
+    appIcon.window.webContents.on('devtools-opened', (event, deviceList, callback) => {
+      appIcon.window.setSize(800, 600);
+      appIcon.window.setResizable(true);
+    });
+
+    appIcon.window.webContents.on('devtools-closed', (event, deviceList, callback) => {
+      appIcon.window.setSize(400, 600);
+      appIcon.window.setResizable(false);
+    });
+
+    ipcMain.on('update-icon', (event, arg) => {
+      if (arg === 'HaveNotifications') {
+        appIcon.setImage(iconHightlightPath);
+      } else {
+        appIcon.setImage(iconTemplatePath);
+      }
+    });
+  };
+
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if(process.platform !== 'darwin') {
